28a0b33d2517f7d6886b559ab3f0d3f9fd9ccbc0
ron <lucifron28@gmail.com>
2024-09-25 23:45:12 +0800

    Access-Control-Allow-Origin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
@@ -34,6 +35,14 @@ banks = [
         type_of_account="joint"
     )
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/banks", response_model=List[Bank])
 def get_banks():
